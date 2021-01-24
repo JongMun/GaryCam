3fa9c521b77ca7e5dacf078138a0590ca935c94c
Jongmun Jeong <smat91@naver.com>
2021-01-25 01:12:42 +0900

    Forth

---
+++ GaryCam/Controller/CaptureViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureViewController.swift
+//  GaryCam
+//
+//  Created by 정종문 on 2021/01/23.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+class CaptureViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
